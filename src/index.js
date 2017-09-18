2a7f9cc95de959cd9dab6b9f66754d84eae549e9
Julio Torres <jtorres@nativapps.co>
2017-09-18 17:09:09 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Lasso from './Lasso';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Lasso />, document.getElementById('root'));
+registerServiceWorker();
